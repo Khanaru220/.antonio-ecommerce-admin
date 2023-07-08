373e19e88d28ebef23f6ea7ab234cc6bf5f562e2
developer <74447462+Khanaru220@users.noreply.github.com>
2023-07-08 16:54:12 +0700

    feat: implement Clerk for auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-	return <p>Hello this is Dashboard</p>;
+import { UserButton } from '@clerk/nextjs';
+
+export default function SetupPage() {
+	return (
+		<div className="p-4">
+			This is a protected route!
+			<UserButton afterSignOutUrl="/  " />
+		</div>
+	);
 }
